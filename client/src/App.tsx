663f1500ff4b39fb3ee2758a3f88feda2d8bf105
ZuchGuillotine <31233203+ZuchGuillotine@users.noreply.github.com>
2025-02-10 06:17:23 +0000

    Assistant checkpoint: Fix blog post navigation
    Assistant generated file changes:
    - client/src/pages/learn/index.tsx: Fix blog post link
    - client/src/App.tsx: Add blog post routes
    Add blog post routes
    
    ---
    
    User prompt:
    
    We have a learn page, that should navigate to an index of blog posts. There are sample blog posts that have been created.  Currently the navigation is not working.  Lets trouble shoot that link

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import React, { Suspense } from "react";
-import { Switch, Route, useLocation } from "wouter";
+import { Route, Switch, BrowserRouter as Router } from "wouter";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/queryClient";
 import { Toaster } from "@/components/ui/toaster";
@@ -137,7 +137,9 @@ function Router() {
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <Router />
+      <Router>
+        <Router />
+      </Router>
       <CookieConsent />
       <Toaster />
     </QueryClientProvider>
